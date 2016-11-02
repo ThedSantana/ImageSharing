436d748df3ae8cd33ae6e5c25a41ae6febcef9d5
creativecoder567 <sarathcreativethinker@gmail.com>
2016-11-02 18:24:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kishore/imagesharing/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.kishore.imagesharing;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+        private ImageView imageView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       imageView=(ImageView)findViewById(R.id.imageView);
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if (type.startsWith("image/")) {
+                handleSendImage(intent);
+            }
+        }
+
+    }
+
+    void handleSendImage(Intent intent) {
+        Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        if (imageUri != null) {
+            imageView.setImageURI(imageUri);
+            }
+    }
+}
